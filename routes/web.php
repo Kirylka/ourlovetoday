a2f10ddbca6c6f28be00882c6f12fe32d13b95d2
Kiryl Anoshka <kiryl.anoshka@itechart-group.com>
2017-01-03 18:22:52 +0300

    init

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/howiloveyou', function () {
+    return view('welcome');
+});
+
+Route::resource('phrases', 'PhrasesController');
