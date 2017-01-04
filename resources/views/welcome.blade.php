0e289811b00fad36f222d07af7dc1035831d4dd3
Kiryl Anoshka <kiryl.anoshka@itechart-group.com>
2017-01-04 15:44:39 +0300

    init

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
             }
             html, body {
                 background-color: #fff;
-                color: #636b6f;
+                color: #3b8445;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
                 height: 100vh;
